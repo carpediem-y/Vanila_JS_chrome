fd796a4dcd6cdcae0a1ad1bdfa38a5b80f9d4d70
carpediem-y <118350576+carpediem-y@users.noreply.github.com>
2024-03-02 21:48:00 +0900

    background 확인

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const images = [
+"background1.jpg",
+"background2.jpg",
+"background3.jpg",
+"background4.jpg",
+"background5.jpg",
+"background6.jpg",
+"background7.jpg",
+"background8.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+url = `../img/${chosenImage}`;
+
+
+document.body.style.backgroundImage = `url(${url})`;
+document.body.style.backgroundRepeat = "no-repeat";
